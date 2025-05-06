ee2fd797ab93f7489e50d1af290c007dbddf713f
David Garzotto <135085722+dgarza123@users.noreply.github.com>
2025-05-06 17:16:22 +0800

    Create 2_Map.py

---
+++ pages/2_Map.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.markdown("""
+<iframe src="https://drive.google.com/file/d/1-8X7o7zX2VfPhJqBSC9fnQk1Y-yRnxpR/preview" 
+width="100%" height="600" allow="autoplay"></iframe>
+""", unsafe_allow_html=True)
